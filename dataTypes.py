46d5e485bada171e5eed10fb600b67a0890e3ec5
akash-code-org <akash.workspace@outlook.com>
2026-06-06 01:31:45 +0500

    Data types in python

---
+++ dataTypes.py
@@ -0,0 +1,23 @@
+# Data types in python:
+
+type1 = 10 
+type2 = "Hello world"
+type3 = 3.34
+type4 = True 
+type5 = [1,2,3]
+type6 = (1,2,3,4)
+type7 = {1,2,3,4,5}
+type8 = {"Name":"Akash","Age":22}
+type9 = 3 + 4j
+print("1. is integer => ",type1)
+print("2. is string => ",type2)
+print("3. is float => ",type3)
+print("4. is boolean => ",type4)
+print("5. is list => ",type5)
+print("6. is tuple => ",type6)
+print("7. is sets => ",type7)
+print("8. is dictionary => ",type8)
+print("9. is complax => ",type9)
+
+# How to check data type systematically
+print(type(type1)) # using type function
