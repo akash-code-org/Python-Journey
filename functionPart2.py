ce2d1cc6e2e42ed30fc0c593320f4a473502e4f3
akash-code-org <akash.workspace@outlook.com>
2026-07-13 22:13:45 +0500

    ..

---
+++ functionPart2.py
@@ -0,0 +1,42 @@
+s = 'hello world'
+print(s.capitalize())
+
+print(s.title())
+
+print(s.upper())
+
+print('HELLO'.lower())
+
+print('HeLiO'.swapcase())
+
+print('my name is nitish'.count('i'))
+
+print('my name is nitish'.find('iss'))
+
+print('my name is nitish'.index('is'))
+
+print('my name is'.startswith('my'))
+
+print('my name is'.endswith('iss'))
+
+name = 'nitish'
+age = 22
+print('my name is {} and age is {}'.format(name,age))
+
+print('name121212'.isalnum())
+
+print('123'.isdigit())
+
+print('nitish'.isalpha())
+
+print('name1'.isidentifier())
+
+print('my name is nitish'.split())
+
+print('-'.join(['my', 'name', 'is', 'nitish']))
+
+print(' '.join(['my', 'name', 'is', 'nitish']))
+
+print('my name is nitish'.replace('nitish','akash'))
+
+print('akash      '.strip())
